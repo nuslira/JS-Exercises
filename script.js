17af232b000c5189bb7872ce818b1198bcad4243
Nusrat <nusratlira8@gmail.com>
2018-05-23 03:00:36 +0000

    Task 5a

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 /* JavaScript code here */
+// Task5
+var cart = [ 
+   {
+       name: 'Shoes',
+       price: 560,
+       quantity: 4
+   },
+   {
+       name: 'Regular Tees',
+       price: 455.50,
+       quantity: 6
+   },
+   {
+       name: 'Socks',
+       price: 65.99,
+       quantity: 2
+   }];
+//5
+function addItem(newItem){
+    cart.push(newItem);
+    console.log(cart);
+}
+var newItem={name:"Watch", price:64, quatity:1};
+addItem();
+
